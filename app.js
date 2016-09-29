9e92eab6f849c0e1697a4ebf6ef77ef39d3869c2
Jordan Tang <jordantang0229@gmail.com>
2016-09-28 19:48:59 -0700

    added function to submit links with posts

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ app.controller('MainCtrl', [
   function($scope) {
     //$scope.test = 'Hello world!';
     $scope.posts = [
-      {title: 'post 1', upvotes: 5},
-      {title: 'post 2', upvotes: 2},
-      {title: 'post 3', upvotes: 15},
-      {title: 'post 4', upvotes: 9},
-      {title: 'post 5', upvotes: 4}
+      {title: 'post 1', link: 'https://www.reddit.com/r/kpop/comments/54ynlx/what_songs_did_you_enjoy_at_first_but_now_dont/', upvotes: 5},
+      {title: 'post 2', link: '', upvotes: 2},
+      {title: 'post 3', link: 'http://i.imgur.com/N1V9NTf.jpg', upvotes: 15},
+      {title: 'post 4', link: 'https://www.reddit.com/r/funny/comments/54w6b0/wake_up_people/', upvotes: 9},
+      {title: 'post 5', link: 'http://imgur.com/a/GQIIw', upvotes: 4}
     ];
     $scope.addPost = function() {
-      $scope.posts.push({title: 'A new post!', upvotes: 0});
+      if(!$scope.title || $scope.title === '') {
+        return;
+      }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
     };
   }]);
